838571eeea511b421a96a733acccdbb1d584882e
Kexuan Yang <kexuan.yang@gmail.com>
2026-04-26 00:05:18 +0800

    Add user local bin to path
    Signed-off-by: Kexuan Yang <kexuan.yang@gmail.com>

--- modules/home/cli/zsh.nix
+++ modules/home/cli/zsh.nix
@@ -20,6 +20,7 @@
     };
 
     initContent = ''
+      export PATH=$PATH:/usr/local/bin
       bindkey "\eh" backward-word
       bindkey "\ej" down-line-or-history
       bindkey "\ek" up-line-or-history
@@ -35,7 +36,6 @@
       fu = "nh os switch --hostname ${profile} --update";
       zu = "sh <(curl -L https://gitlab.com/Zaney/zaneyos/-/releases/latest/download/install-zaneyos.sh)";
       ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
-      cat = "bat";
       man = "batman";
       gst = "git status";
       gd = "git diff";
