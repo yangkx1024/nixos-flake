d183cfef89f80597109c4c8b2fb1968a11c3ecf5
Kexuan Yang <kexuan.yang@gmail.com>
2026-04-25 23:24:51 +0800

    Fix cursor theme
    Signed-off-by: Kexuan Yang <kexuan.yang@gmail.com>

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -9,6 +9,10 @@
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
     };
+    cursorTheme = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+    };
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
     };
