ad5c8a7065987c74c1b2615842ee3f89dadec11e
Kexuan Yang <kexuan.yang@gmail.com>
2026-06-12 12:45:04 +0800

    Fix qt lib issue
    Signed-off-by: Kexuan Yang <kexuan.yang@gmail.com>

--- modules/core/qt.nix
+++ modules/core/qt.nix
@@ -16,7 +16,7 @@
     # alternate options
     # libsForQt5.qt5compat
     kdePackages.qt5compat
-    libsForQt5.qt5.qtgraphicaleffects
+    qt5.qtgraphicaleffects
   ];
 
   # necessary environment variables
