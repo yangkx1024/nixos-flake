d57f7ecd2150f312e111de24bd0f87070fac274a
Kexuan Yang <kexuan.yang@gmail.com>
2026-08-27 14:12:15 +0800

    Optimize nfs mount config
    Signed-off-by: Kexuan Yang <kexuan.yang@gmail.com>

--- modules/core/nfs.nix
+++ modules/core/nfs.nix
@@ -5,7 +5,11 @@
     {
       type = "nfs";
       mountConfig = {
-        Options = "noatime";
+        # retry=0 + a short TimeoutSec: fail fast instead of blocking for the
+        # default 90s when the NAS is offline, so `ls`/Thunar return an error
+        # rather than appearing hung.
+        Options = "noatime,retry=0";
+        TimeoutSec = "10s";
       };
       what = "192.168.1.9:/mnt/hdd/shared";
       where = "/home/${username}/nas";
